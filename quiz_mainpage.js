8ba723e09ade89ccf2d9e430d26a4d42eefbf8bd
RithvikaCheela <90195216+RithvikaCheela@users.noreply.github.com>
2022-02-14 15:08:11 +0530

    Add files via upload

--- quiz_mainpage.js
+++ quiz_mainpage.js
@@ -5,8 +5,8 @@ function send(){
 
     question_no="<h4>" + no1 + " X " + no2 + "</h4";
 
-    input_box="<br>Answer: <input type='text' id='input_check_box'>";
-    check_button="<br><button class='btn btn-info' onclick='check()'> Check </button>";
+    input_box="<br><br><center>Answer: <input type='text' id='input_check_box'>";
+    check_button="<br><br><button class='btn btn-info' onclick='check()'> Check </button></center>";
 
     row=question_no+input_box+check_button;
     document.getElementById("output").innerHTML=row;
